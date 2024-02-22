c49aba5bac2aabb8e6440aa04fc0b79f062623ca
hakan-yigit <@hakan-yigit>
2024-02-22 21:14:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import "./styles.css";
+import housesForSale from "./data/housesForSale";
+import HouseCard from "./data/HouseCard";
+
+
+export default function App() {
+  /* Challenge
+  
+      Aşağıdaki map metodu tarafından oluşturulan houseCard'ların JSX'inin ayrı bir fonksiyonel bileşene yerleştirilmesi gerekiyor. Göreviniz bunu aşağıdaki gibi yapmaktır:
+      
+          1. Aşağıdaki map metodunun return ifadesindeki mevcut JSX, sizin oluşturmanız gereken bir <HouseCard /> bileşeninin tek bir örneği ile değiştirilmelidir. 
+             
+          2. HouseCard bileşeni, şu anda map metodu içinde döndürülen JSX ile aynı JSX'i döndürmelidir. 
+      
+      3. JSX'teki değişkenler, map metodunda döndüreceğiniz <HouseCard /> bileşenine aktarılması gereken prop'lar haline gelmelidir. 
+         
+      4. HouseCard bileşeni, bileşenler klasöründe ayrı bir dosyaya yerleştirilmeli ve bu App.jsx dosyasına aktarılmalıdır.
+          
+          5. <HouseCard /> içine yalnızca en fazla dört prop geçirmelisiniz. HouseData'nın her parçasını ayrı bir prop olarak manuel olarak geçirmemelisiniz. Bu görevleri tamamladıktan sonra site tamamen aynı görünmeli ve davranmalıdır. Buradaki challenge sadece " altta yatan" kodu geliştirmekle ilgilidir.
+         
+      Bonus Challenge: Sırasız listedeki liste öğelerini manuel olarak (şu anda oluşturuldukları gibi) değil, yinelemeli olarak (yani, bir tür yinelemeli yöntem/yaklaşım kullanarak) oluşturun.
+  */
+
+  //YİNELEMELİ
+  
+  const houseCards = []
+  
+  for(let i = 0 ; i<housesForSale.length ; i++){
+    const houseData = housesForSale[i]
+    houseCards.push(<HouseCard houseData={houseData} key={houseData.id} index={i} array={housesForSale} />)
+  }
+
+
+
+
+
+
+
+  //MANUEL
+  
+  // const houseCards = housesForSale.map((houseData, index, array) => {
+  //   return <HouseCard houseData={houseData} key={houseData.id} index={index} array={array} />;
+  // });
+
+  return (
+    <div className="wrapper">
+      <header>
+        <img className="logo" src="images/logo.png" />
+      </header>
+      <div className="house-cards-container">{houseCards}</div>
+    </div>
+  );
+}
